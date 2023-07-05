cf8612d427b82e31f1bfa83b0d31875446173b6b
dariofusco <fdario@hotmail.it>
2023-07-05 16:57:11 +0200

    esegue esercizio mail

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const listMail = ["mariorossi@mail.it", "giuseppeverdi@mail.it", "pippo@mail.it", "pluto@mail.it"];
+let accessAllowed = false;
+
+const searchButton = document.getElementById("button")
+searchButton.addEventListener("click", function () {
+    mailToSearch = document.getElementById("mail").value
+    console.log(mailToSearch)
+
+    for (let i = 0; i < listMail.length; i++) {
+        const allMail = listMail[i];
+        console.log(allMail);
+        
+        if (allMail === mailToSearch) {
+            console.log("trovata");
+            accessAllowed = true;
+        }
+        if (accessAllowed = true) {
+            document.getElementById("access").innerHTML = "Accesso consentito";
+        } else {
+            document.getElementById("access").innerHTML = "Accesso non consentito";
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
